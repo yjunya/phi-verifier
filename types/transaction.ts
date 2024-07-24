6f4e8ff7e5bdbbff3afe081c7374479e5d3183b3
yjunya <38554985+yjunya@users.noreply.github.com>
2024-07-24 23:49:55 +0900

    Verify deploy a smart contract on eth and sepolia (#1)
    * Add etherscan api key
    
    * Add transaction type
    
    * Impl deploy-a-smart-contract verifier

---
+++ types/transaction.ts
@@ -0,0 +1,22 @@
+export type Transaction = {
+  blockNumber: string;
+  blockHash: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  transactionIndex: string;
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  gasPrice: string;
+  input: string;
+  methodId: string;
+  functionName: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  txreceipt_status: string;
+  gasUsed: string;
+  confirmations: string;
+  isError: string;
+};
